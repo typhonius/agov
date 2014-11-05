48151359d7cbbf2a7783896cf60fc39f22d1d19a
JohnAlbin <virtually.johnalbin@gmail.com>
2014-11-05 18:07:13 +0800

    [#27106] Update to Compass 1.0.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
 source 'https://rubygems.org'
 
-gem 'breakpoint', '~> 2.0.5'
-gem 'compass', '~> 0.12.2'
-gem 'sass-globbing', '~> 1.1.1'
+gem 'breakpoint',     '~> 2.5.0'
+gem 'compass',        '~> 1.0.1'
+# Build compass sprites faster with oily_png.
+gem 'oily_png',       '~> 1.1.2'
+gem 'sass',           '~> 3.4.7'
+gem 'sass-globbing',  '~> 1.1.1'
+gem 'zen-grids',      '~> 2.0.0.beta.3'
--- themes/agov/AGOV_STARTERKIT/config.rb
+++ themes/agov/AGOV_STARTERKIT/config.rb
@@ -7,45 +7,21 @@
 #
 
 
-# Change this to :production when ready to deploy the CSS to the live server.
-environment = :development
-#environment = :production
-
-# In development, we can turn on the FireSass-compatible debug_info.
-firesass = false
-#firesass = true
-
+# Require any additional compass plugins installed on your system.
+require 'breakpoint'
+require 'sass-globbing'
+require 'zen-grids'
 
 # Location of the theme's resources.
-http_path       = "/"
 css_dir         = "css"
 sass_dir        = "sass"
 fonts_dir       = "fonts"
-extensions_dir  = "sass-extensions"
 images_dir      = "images"
 javascripts_dir = "js"
 
-
-# Require any additional compass plugins installed on your system.
-#require 'ninesixty'
-#require 'zen-grids'
-require 'breakpoint'
-require 'sass-globbing'
-
-# Assuming this theme is in sites/*/themes/THEMENAME, you can add the partials
-# included with a module by uncommenting and modifying one of the lines below:
-#add_import_path "../../../default/modules/FOO"
-#add_import_path "../../../all/modules/FOO"
-#add_import_path "../../../../modules/FOO"
-
-
-##
-## You probably don't need to edit anything below this.
-##
-
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = (environment == :development) ? :expanded : :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Since Drupal
 # themes can be installed in multiple locations, we don't need to worry about
@@ -55,6 +31,8 @@ relative_assets = true
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
 
-# Pass options to sass. For development, we turn on the FireSass-compatible
-# debug_info if the firesass config variable above is true.
-sass_options = (environment == :development && firesass == true) ? {:debug_info => true} : {}
+# CSS sourcemaps
+sourcemap = true
+
+# Pass options to sass.
+# sass_options = {}
